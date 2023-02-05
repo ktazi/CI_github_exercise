f1362df0a9d3ca54ce86b6dc00054ac2b91f9d11
ktazi <56438181+ktazi@users.noreply.github.com>
2023-02-05 17:10:09 +0100

    Add files via upload

---
+++ flask_api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api
+from flask_cors import CORS
+from tensorflow import keras
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+model = keras.models.load_model("model.h5")
+@app.route('/api/classify/')
+def test():
+    x = np.array(request.args["img"].split(",")).astype(int).reshape(1,28,28)
+    predictions = list(model.predict(x)[0].astype(np.int32))
+    return jsonify(int(np.argmax(predictions)))
+
+if __name__ == "__main__":
+    app.run(port=5000)
---
+++ model_build.py
@@ -0,0 +1,19 @@
+from tensorflow import keras
+import pandas as pd
+from keras.callbacks import EarlyStopping
+
+#create the model
+model = keras.Sequential([keras.layers.Flatten(input_shape=(28,28)),
+                        keras.layers.Dense(128, activation='relu', input_shape=(28,28)), 
+                        keras.layers.Dense(10, activation='softmax')])
+model.compile(loss='binary_crossentropy', optimizer="adam", metrics=['accuracy'])
+print(model.summary())
+#import train dataset
+df = pd.read_csv("fashion-mnist-train-1.csv")
+df[df.columns[1:]] = df[df.columns[1:]] / 255 # normalization
+X = df[df.columns[1:]].values.reshape((14999, 28,28))
+Y = keras.utils.to_categorical(df["label"].values)
+print(X.shape,Y.shape)
+es = EarlyStopping(monitor='accuracy', mode='auto', verbose=1, patience=20)
+model.fit(X,Y, epochs=70,batch_size=800, verbose=1,callbacks=[es])
+model.save("model.h5")
